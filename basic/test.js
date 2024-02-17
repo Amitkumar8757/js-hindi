f9d8215d1d6bc674a8bbfb8ce80d02cb62cb8084
Amitkumar8757 <86163370+Amitkumar8757@users.noreply.github.com>
2024-02-17 04:34:17 +0000

    variable

--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,19 @@
-console.log("Amit");
+console.log("Amit")
+const AccountId =1234
+var AccountName="Amit"
+let AccountPassword="12345"
+AccountCity="Delhi"
+
+
+
+AccountName="bittu"
+AccountPassword="234567"
+AccountCity="Noida"
+let accountstate
+
+//AccountId=2 not work due to AccountId is constant value it never change
+console.log(AccountId);
+console.table([AccountId, AccountName, AccountPassword, accountstate])
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
